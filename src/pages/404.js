1776ff7cbfbe9ffccf95fb3b65f2bbc76d362100
gixxerblade <sdclarkie@gmail.com>
2020-02-28 13:03:33 -0500

    Updated NotFound page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,28 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
-const NotFoundPage = () => (
-  <Layout>
-    <div className="not-found-wrapper">
-      <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p className="not-found-text">
-        You just hit a route that doesn&#39;t exist...
-        <span className="sadface" role="img" aria-label="sad">
-          😪
-        </span>
-      </p>
-    </div>
-  </Layout>
-)
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <div className="not-found-wrapper">
+        <SEO title="404: Not found" />
+        <h1>NOT FOUND</h1>
+        <div className="not-found-text">
+          <p>
+            You just hit a route that doesn&#39;t exist...
+            <span className="sadface" role="img" aria-label="sad">
+              😪
+            </span>
+          </p>
+          <p>
+            Go back to the <Link to="/">Home</Link> page
+          </p>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
