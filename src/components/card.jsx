8e2ccd3a2ca5b810118e0fc73d5c7a522db02566
gixxerblade <sdclarkie@gmail.com>
2020-04-06 11:56:56 -0400

    added another project to projects page

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,6 +1,4 @@
 import React from "react"
-//import PropTypes from "prop-types"
-//import { withStyles } from "@material-ui/core/styles"
 import Card from "@material-ui/core/Card"
 import CardActionArea from "@material-ui/core/CardActionArea"
 import CardActions from "@material-ui/core/CardActions"
@@ -16,7 +14,7 @@ const text = {
   project1: {
     title: "Angry Pickles",
     description:
-      "A minimal ecommerce application to create a storefront with Gatsby, Stripe, & Netlify Functions. The site is statically generated based on Stripe inventory and dynamically updates with live inventory & availability data. The checkout is powered by Stripe integration with serverless functions to interact with the Stripe API. The shopping cart persists in local storage and responsive images provided with gatsby-image.",
+      "An ecommerce application to create a storefront with Gatsby, Stripe, & Netlify Functions.",
     source: "https://github.com/gixxerblade/Angry-Pickles",
     link: "https://angrypickles.netlify.com/",
     images: "https://i.ibb.co/pdv2kRk/angrypickle.jpg",
@@ -24,17 +22,29 @@ const text = {
   project2: {
     title: "Create, Read, Update, and Delete Tutorial Application",
     description:
-      'Created an application to demonstrate a "how-to" using Gatsby and React, utilizing Google Firestore NoSQL database for data storage.',
+      'Created an application to demonstrate persistent storage using Gatsby and React, utilizing Google Firestore for data storage.',
     source:
       "https://dev.to/vetswhocode/build-a-crud-firestore-app-in-react-gatsby-with-hooks-4ig9",
     link: "https://codesandbox.io/s/gatsby-crud-tutorial-c6xs1",
     images: "https://i.ibb.co/QdL9MW7/editing-Works.gif",
   },
+  project3: {
+    title: "Profile Card Creator",
+    description:
+      "I wrote a tutorial for dev.to upload and fetch photos using Cloudinary",
+    source:
+      "https://dev.to/vetswhocode/react-tutorial-upload-and-fetch-photos-with-cloudinary-2ec9",
+    link: "http://profile-card-tut.surge.sh/",
+    images: "https://i.ibb.co/fQTW1M0/vwc.jpg",
+  },
 }
 
 const useStyles = makeStyles(theme => ({
   card: {
-    maxWidth: 700,
+    maxWidth: 400,
+  },
+  cardContent: {
+    height: 170,
   },
   media: {
     height: 200,
@@ -42,18 +52,19 @@ const useStyles = makeStyles(theme => ({
       height: 0,
     },
     [theme.breakpoints.down("sm")]: {
-      height: 100,
+      height: 0,
     },
   },
   typography: {
     fontSize: 10,
-  }
+    [theme.breakpoints.down("sm")]: {
+      fontSize: 8,
+    }
+  },
 }))
 const MediaCard = props => {
   const classes = useStyles(props)
-  console.log(text)
   const items = Object.entries(text).map((key, value) => {
-    console.log(key[1].title)
     return (
       <StyledCardDiv key={key}>
         <Card className={classes.card}>
@@ -63,12 +74,14 @@ const MediaCard = props => {
               image={key[1].images}
               title={key[1].title}
             />
-            <StyledCardContent>
-              <Typography gutterBottom variant="h5" component="h2">
+            <CardContent className={classes.cardContent}>
+              <Typography gutterBottom variant="h6" component="h2">
                 {key[1].title}
               </Typography>
-              <Typography fontSize={10} component="p">{key[1].description}</Typography>
-            </StyledCardContent>
+              <Typography fontSize={classes.typography} component="p">
+                {key[1].description}
+              </Typography>
+            </CardContent>
           </CardActionArea>
           <CardActions>
             <Button href={key[1].source} size="small" color="primary">
@@ -98,11 +111,4 @@ export default MediaCard
 const StyledCardDiv = styled.div`
   margin: 1rem;
   list-style-type: none;
-`
-const StyledCardContent = styled(CardContent)`
-  height: 15rem;
-  @media only screen and (min-width: 320px) and (max-width: 480px) {
-    width: 100%;
-    height: 12rem;
-  }
 `
